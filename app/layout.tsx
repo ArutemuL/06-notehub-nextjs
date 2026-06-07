90483321a417b41b432924fad1402ec164eecffc
ArutemuL <lvovartem002@gmail.com>
2026-06-07 22:14:49 +0300

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
-import "modern-normalize";
 import "@/global.css";
 import Header from "@/components/Header/Header";
 import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
